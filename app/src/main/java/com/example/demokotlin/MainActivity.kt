c2b3161d40bb1fdd46bccdb522bb3c9760d17385
Administrator <vsubhash817@gmail.com>
2023-02-17 17:58:49 +0530

    push notification kotlin

--- app/src/main/java/com/example/demokotlin/MainActivity.kt
+++ app/src/main/java/com/example/demokotlin/MainActivity.kt
@@ -1,8 +1,19 @@
 package com.example.demokotlin
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.RemoteViews
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import com.example.demokotlin.api.QuoteService
@@ -12,10 +23,73 @@ import com.example.demokotlin.viewmodel.MainViewModel
 import com.example.demokotlin.viewmodel.MainViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+    // declaring variables
+    lateinit var notificationManager: NotificationManager
+    lateinit var notificationChannel: NotificationChannel
+    lateinit var builder: Notification.Builder
+    private val channelId = "i.apps.notifications"
+    private val description = "Test notification"
+
+
     lateinit var mainViewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // accessing button
+        val btn = findViewById<Button>(R.id.btn)
+
+        // it is a class to notify the user of events that happen.
+        // This is how you tell the user that something has happened in the
+        // background.
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // onClick listener for the button
+        btn.setOnClickListener {
+
+            // pendingIntent is an intent for future use i.e after
+            // the notification is clicked, this intent will come into action
+            val intent = Intent(this, PushNotificationActivity::class.java)
+
+            // FLAG_UPDATE_CURRENT specifies that if a previous
+            // PendingIntent already exists, then the current one
+            // will update it with the latest intent
+            // 0 is the request code, using it later with the
+            // same method again will get back the same pending
+            // intent for future reference
+            // intent passed here is to our afterNotification class
+            val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+            // RemoteViews are used to use the content of
+            // some different layout apart from the current activity layout
+            val contentView = RemoteViews(packageName, R.layout.activity_push_notification)
+
+            // checking if android version is greater than oreo(API 26) or not
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                notificationChannel = NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_HIGH)
+                notificationChannel.enableLights(true)
+                notificationChannel.lightColor = Color.GREEN
+                notificationChannel.enableVibration(false)
+                notificationManager.createNotificationChannel(notificationChannel)
+
+                builder = Notification.Builder(this, channelId)
+                    .setContent(contentView)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_launcher_background))
+                    .setContentIntent(pendingIntent)
+            } else {
+
+                builder = Notification.Builder(this)
+                    .setContent(contentView)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_launcher_background))
+                    .setContentIntent(pendingIntent)
+            }
+            notificationManager.notify(1234, builder.build())
+        }
+
+
+
         val txtQuote = findViewById<TextView>(R.id.txtQuote)
 
         val quoteService=RetrofitHelper.getInstance().create(QuoteService::class.java)
---
+++ app/src/main/java/com/example/demokotlin/PushNotificationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.demokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PushNotificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_push_notification)
+    }
+}
